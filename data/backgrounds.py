3457c0b53e2148ae8554abacf13f39bcd61b93dd
Evenweaker <gilera3@gmail.com>
2026-07-27 22:21:11 +0100

    Add backgrounds data

---
+++ data/backgrounds.py
@@ -0,0 +1,14 @@
+BACKGROUNDS = {
+    "Acolyte": {"skills": ["Insight", "Religion"], "languages": 2, "equipment": "Holy symbol, prayer book, 5 sticks of incense, vestments, common clothes, 15 gp"},
+    "Criminal": {"skills": ["Deception", "Stealth"], "tool": "Thieves' tools, gaming set", "equipment": "Crowbar, dark common clothes with hood, 15 gp"},
+    "Folk Hero": {"skills": ["Animal Handling", "Survival"], "tool": "Artisan's tools, vehicles (land)", "equipment": "Artisan's tools, shovel, iron pot, common clothes, 10 gp"},
+    "Noble": {"skills": ["History", "Persuasion"], "tool": "Gaming set", "languages": 1, "equipment": "Fine clothes, signet ring, scroll of pedigree, 25 gp"},
+    "Sage": {"skills": ["Arcana", "History"], "languages": 2, "equipment": "Bottle of ink, quill, small knife, letter from dead colleague, common clothes, 10 gp"},
+    "Soldier": {"skills": ["Athletics", "Intimidation"], "tool": "Gaming set, vehicles (land)", "equipment": "Insignia of rank, trophy, gaming set, common clothes, 10 gp"},
+    "Charlatan": {"skills": ["Deception", "Sleight of Hand"], "tool": "Disguise kit, forgery kit", "equipment": "Fine clothes, disguise kit, tools of the con, 15 gp"},
+    "Entertainer": {"skills": ["Acrobatics", "Performance"], "tool": "Disguise kit, musical instrument", "equipment": "Musical instrument, favor of admirer, costume, 15 gp"},
+    "Guild Artisan": {"skills": ["Insight", "Persuasion"], "tool": "Artisan's tools", "languages": 1, "equipment": "Artisan's tools, letter of introduction, traveler's clothes, 15 gp"},
+    "Hermit": {"skills": ["Medicine", "Religion"], "tool": "Herbalism kit", "languages": 1, "equipment": "Scroll case with notes, winter blanket, common clothes, herbalism kit, 5 gp"},
+    "Outlander": {"skills": ["Athletics", "Survival"], "tool": "Musical instrument", "languages": 1, "equipment": "Staff, hunting trap, trophy, traveler's clothes, 10 gp"},
+    "Sailor": {"skills": ["Athletics", "Perception"], "tool": "Navigator's tools, vehicles (water)", "equipment": "Belaying pin, silk rope, lucky charm, common clothes, 10 gp"}
+}
